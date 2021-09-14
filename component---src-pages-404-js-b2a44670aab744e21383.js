68b5013b838b0069b4ced0e23bc00251dde59b36
HunterRk <renaldyhidayatt.com>
2021-09-14 17:32:43 +0700

    Updates

---
+++ component---src-pages-404-js-b2a44670aab744e21383.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[883],{9616:function(e,t,n){n.r(t);var u=n(7294),a=n(2e3),l=n(3548);t.default=function(){return u.createElement(a.Z,null,u.createElement(l.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-b2a44670aab744e21383.js.map
